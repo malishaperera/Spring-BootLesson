d9071142e9fa78fb622d5b48d4a1e2db1a85f2b6
Malisha Perera <140098754+malishaperera@users.noreply.github.com>
2024-10-13 13:28:47 +0530

    Add AuthenticationProvider

--- src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/config/SecurityConfig.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/config/SecurityConfig.java
@@ -5,10 +5,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,5 +33,27 @@ public class SecurityConfig {
                 .authenticationProvider()
                 .addFilterBefore();
         return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    //Authentication Provider
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider dap =
+                new DaoAuthenticationProvider();
+        dap.setUserDetailsService();
+        dap.setPasswordEncoder(passwordEncoder());
+        return dap;
+    }
+
+    //AuthenticationManager
+    @Bean
+    public AuthenticationManager authenticationManager (AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 }
