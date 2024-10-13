ccee9e058a2bd63fcec0a5cd85aaa275ca9d34f7
Malisha Perera <140098754+malishaperera@users.noreply.github.com>
2024-10-13 16:24:50 +0530

    Update the UserEntity

--- src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/config/JWTConfig.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/config/JWTConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -48,8 +49,11 @@ public class JWTConfig extends OncePerRequestFilter {
                     userService.userDetailsService().loadUserByUsername(useEmail);
             if(jwtService.isTokenValid(jwToken, loadedUser)) {
                 SecurityContext emptyContext = SecurityContextHolder.createEmptyContext();
-                UsernamePasswordAuthenticationToken newContext =
+                UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(loadedUser, null, loadedUser.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetails(request));
+                emptyContext.setAuthentication(authToken);
+                SecurityContextHolder.setContext(emptyContext);
             }
 
         }
--- src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/entity/UserEntity.java
+++ src/main/java/lk/ijse/gdse/aad68/NoteCollectorV2/entity/UserEntity.java
@@ -6,11 +6,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 
 @AllArgsConstructor
@@ -28,16 +27,40 @@ public class UserEntity implements UserDetails ,SuperEntity{
     @Column(columnDefinition = "LONGTEXT")
     private String profilePicture;
     @OneToMany(mappedBy = "user")
-    private List<NoteEntity> notes = new ArrayList<>();
+    private List<NoteEntity> notes;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_"+role.name()));
+        return authorities;
     }
 
     @Override
     public String getUsername() {
-        return "";
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
